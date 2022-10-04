e75001384f7e3d303ea5b9c3a7aa84cbccbd5624
이소영 <th42500@naver.com>
2022-10-04 13:21:06 +0900

    Add ArrayTest.java
    배열 테스트 추가

---
+++ src/ArrayTest.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        // 10칸의 int array를 만들고
+        int[] arr = new int[10];
+        // 1부터 10까지 채워보세요
+        for (int i = 1; i <= 10; i++) {
+            arr[i-1] = i;
+        }
+        // 그리고 console에 출력해보세요.
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
