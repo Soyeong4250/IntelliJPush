45acb204267b56bd06cfa8d4774177602ab70d46
이소영 <th42500@naver.com>
2022-10-04 14:45:02 +0900

    50000 ~ 10까지 주어진 금액 나누고 남은 금액 출력

--- src/CoinTest.java
+++ src/CoinTest.java
@@ -11,13 +11,7 @@ public class CoinTest {
         System.out.print("금액을 넣으세요 : ");
         int coin = sc.nextInt();
 
-        /*int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-
-        for (int i = 0; i < 6; i++) {
-            System.out.printf("%d원 : %d개\n", unit[i], coin/unit[i]);
-            coin %= unit[i];
-        }*/
-
+        // 1단계. 각 화폐 단위를 변수로 지정하고, 주어진 금액을 각 화폐 단위로 환산 & 나머지 출력
         int curr50000 = 50000;
         int curr10000 = 10000;
         int curr5000 = 5000;
@@ -25,12 +19,31 @@ public class CoinTest {
         int curr500 = 500;
         int curr100 = 100;
 
-        System.out.printf("50000원권 : %d장 나머지 : %d원\n", coin/curr50000, coin % curr50000);
+        /*System.out.printf("50000원권 : %d장 나머지 : %d원\n", coin/curr50000, coin % curr50000);
         System.out.printf("10000원권 : %d장 나머지 : %d원\n", coin/curr10000, coin % curr10000);
         System.out.printf("5000원권 : %d장 나머지 : %d원\n", coin/curr5000, coin % curr5000);
         System.out.printf("1000원권 : %d장 나머지 : %d원\n", coin/curr1000, coin % curr1000);
         System.out.printf("500원 동전 : %d개 나머지 : %d원\n", coin/curr500, coin % curr500);
         System.out.printf("100원 동전 : %d개 나머지 : %d원\n", coin/curr100, coin % curr100);
+        */
+
+        // 2단계. 주어진 금액을 각 화폐 단위를 활용하여 최소의 개수로 환산하기
+        System.out.printf("50000원권 : %d장 나머지 : %d원\n", coin/curr50000, coin % curr50000);
+        coin %= curr50000;
+        System.out.printf("10000원권 : %d장 나머지 : %d원\n", coin/curr10000, coin % curr10000);
+        coin %= curr10000;
+        System.out.printf("5000원권 : %d장 나머지 : %d원\n", coin/curr5000, coin % curr5000);
+        coin %= curr5000;
+        System.out.printf("1000원권 : %d장 나머지 : %d원\n", coin/curr1000, coin % curr1000);
+        coin %= curr1000;
+        System.out.printf("500원 동전 : %d개 나머지 : %d원\n", coin/curr500, coin % curr500);
+        coin %= curr500;
+        System.out.printf("100원 동전 : %d개 나머지 : %d원\n", coin/curr100, coin % curr100);
+        coin %= curr100;
+        System.out.printf("50원 동전 : %d개 나머지 : %d원\n", coin/50, coin % 50);
+        coin %= 50;
+        System.out.printf("10원 동전 : %d개 나머지 : %d원\n", coin/10, coin % 10);
+        coin %= 10;
     }
 
 }
