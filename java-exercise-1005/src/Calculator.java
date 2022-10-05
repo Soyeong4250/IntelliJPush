3c121ba565762342f2fce76c6c9d30dde7e6da00
이소영 <th42500@naver.com>
2022-10-05 10:49:05 +0900

    생성자 생성 & 매개변수 없애기

--- java-exercise-1005/src/Calculator.java
+++ java-exercise-1005/src/Calculator.java
@@ -1,22 +1,30 @@
 public class Calculator {
 
-    public void plus (int a, int b) {
+    private int a;
+    private int b;
+    public Calculator(){};
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public void plus () {
         System.out.println(a + b);
     }
 
-    public void minus (int a, int b) {
+    public void minus () {
         System.out.println(a - b);
     }
 
-    public void multiple (int a, int b) {
+    public void multiple () {
         System.out.println(a * b);
     }
 
-    public void divide (int a, int b) {
+    public void divide () {
         System.out.println(a / b);
     }
 
-    public void divide2(int a, int b) {
+    public void divide2() {
         System.out.println((float)a/b);
     }
 }
--- java-exercise-1005/src/CalculatorMain.java
+++ java-exercise-1005/src/CalculatorMain.java
@@ -1,12 +1,12 @@
 public class CalculatorMain {
 
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        cal.plus(10, 20);  // 30
-        cal.minus(20, 10);  // 10
-        cal.multiple(10, 20);  // 200
-        cal.divide(10, 20);  // 0
-        cal.divide2(10, 20);  // 0.5
+        Calculator cal = new Calculator(10, 20);
+        cal.plus();  // 30
+        cal.minus();  // 10
+        cal.multiple();  // 200
+        cal.divide();  // 0
+        cal.divide2();  // 0.5
     }
 
 }
