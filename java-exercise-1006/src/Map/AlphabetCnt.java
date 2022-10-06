f7a8f4495413d9f1dcbedbed6da70e66ad474799
이소영 <th42500@naver.com>
2022-10-06 13:49:45 +0900

    A~Z까지 등록한 후 개수 세기

--- java-exercise-1006/src/Map/AlphabetCnt.java
+++ java-exercise-1006/src/Map/AlphabetCnt.java
@@ -5,15 +5,25 @@ import java.util.HashMap;
 public class AlphabetCnt {
 
     public static void main(String[] args) {
-        String s1 = "aabbccddefghijkkkkkllm";
+        AlphabetCnt alphabetCnt = new AlphabetCnt();
+        String s1 = "aabbccddefghijkkkkkllm".toLowerCase();
         HashMap<Character, Integer> hash = new HashMap<>();
+
+        for (int c = 'A'; c <= 'Z' ; c++) {
+            hash.put(Character.toLowerCase((char)c), 0);
+        }
+
         for(int i=0; i < s1.length(); i++) {
 //          System.out.println(s1.charAt(i));
-            char ch = s1.charAt(i);
-            if(isAlphabet(ch)) {
-                hash.put(ch, hash.getOrDefault(ch, 0) + 1);
+            char c = s1.charAt(i);
+            boolean isAlphabet = alphabetCnt.isAlphabet(c);
+
+            // 여기에서만 개수를 센다
+            if(isAlphabet) {
+                hash.put(c, hash.getOrDefault(c, 0)+1);
             }
         }
+
 
         System.out.println("사용된 알파벳의 개수는 " + hash.size());
         System.out.println("각 알파벳의 개수는 다음과 같습니다.");
@@ -21,7 +31,7 @@ public class AlphabetCnt {
 
     }
 
-    public static boolean isAlphabet(char c) {
+    public boolean isAlphabet(char c) {
         if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
             return true;
         }
