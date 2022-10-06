59d46f03389db269bdb8db3a08ae0fe1e4e13019
이소영 <th42500@naver.com>
2022-10-06 23:43:03 +0900

    Add Homework (1021~1030)

---
+++ CodeUp/src/Main_1028.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main_1028 {  // [기초-데이터형] 정수 1개 입력받아 그대로 출력하기2
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long num = sc.nextLong();
+        System.out.println(num);
+        sc.close();
+    }
+}
