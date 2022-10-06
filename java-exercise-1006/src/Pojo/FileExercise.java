6e88ccd775a1cc695ec0102aefd9c3facafd1edf
이소영 <th42500@naver.com>
2022-10-06 15:21:46 +0900

    상위 디렉토리 파일 목록 출력

--- java-exercise-1006/src/Pojo/FileExercise.java
+++ java-exercise-1006/src/Pojo/FileExercise.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public class FileExercise {
     public static void main(String[] args) {
-        // 현재 디렉토리 파일 목록 출력
-        File dir = new File("./");
+        // 상위 디렉토리 파일 목록 출력
+        File dir = new File("../");
         File files[] = dir.listFiles();
 
         for (File file : files) {
@@ -14,10 +14,12 @@ public class FileExercise {
     }
 }
 
+
 /*
-.\.idea
-.\a_file.txt
-.\java-exercise-1006.iml
-.\out
-.\src
+상위 디렉토리의 파일 목록 출력
+..\.git
+..\CodeUp
+..\java-exercise-1004
+..\java-exercise-1005
+..\java-exercise-1006
 */
