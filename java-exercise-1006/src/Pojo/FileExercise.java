f0e766c65c50db150e8e4ab7c6af5f38f42ab848
이소영 <th42500@naver.com>
2022-10-06 15:30:42 +0900

    텍스트 파일 읽고 한글자 출력

--- java-exercise-1006/src/Pojo/FileExercise.java
+++ java-exercise-1006/src/Pojo/FileExercise.java
@@ -1,16 +1,30 @@
 package Pojo;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileExercise {
-    public static void main(String[] args) {
+    public void printFiles() {
         // 상위 디렉토리 파일 목록 출력
         File dir = new File("../");
-        File files[] = dir.listFiles();
+        File[] files = dir.listFiles();
 
         for (File file : files) {
             System.out.println(file);
         }
+    }
+    public char readAChar (String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+
+        return (char) fileReader.read();
+    }
+
+    public static void main(String[] args) throws IOException {
+       FileExercise fileExercise = new FileExercise();
+       char c = fileExercise.readAChar("./src/Pojo/a_file.txt");
+
+        System.out.println(c);
     }
 }
 
