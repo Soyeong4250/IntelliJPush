d056cc46526b418538b4733d74cffc7dfcb10a35
이소영 <th42500@naver.com>
2022-10-06 10:11:37 +0900

    Add : Add Set Practice
    랜덤으로 숫자를 50개 생성하고 Set을 이용하여 중복제거

---
+++ java-exercise-1006/src/Set/RandomNumberGenerator.java
@@ -0,0 +1,10 @@
+package Set;
+
+public class RandomNumberGenerator implements NumberGenerator{
+
+    @Override
+    public int generate(int num) {
+        return (int)(Math.random()*num);
+    }
+
+}
