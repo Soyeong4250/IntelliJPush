a1aa79c033775aa688675c17993452950da99823
이소영 <th42500@naver.com>
2022-10-06 10:23:26 +0900

    Refactor : Edit RndNumbersWithoutDuplicated.java
    LikedHashSet을 HashSet으로 변경

--- java-exercise-1006/src/Set/RndNumbersWithoutDuplicated.java
+++ java-exercise-1006/src/Set/RndNumbersWithoutDuplicated.java
@@ -1,21 +1,20 @@
 package Set;
 
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 
 public class RndNumbersWithoutDuplicated {
 
     public static void main(String[] args) {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();  // 입력 순서대로 데이터 관리
+        HashSet<Integer> hash = new HashSet<>();
         for (int i = 0; i < 50; i++) {
-
             int r = randomNumberGenerator.generate(10);  // 10미만의 랜덤한 숫자 생성
             // System.out.println(r);
 
-            linkedHashSet.add(r);
+            hash.add(r);
         }
 
-        System.out.println(linkedHashSet);
+        System.out.println(hash);
     }
 
 }
