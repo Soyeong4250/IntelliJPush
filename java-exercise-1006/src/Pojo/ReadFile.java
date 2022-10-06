f6f18bb76b6c558daf64fbc751aaa168bf5f496d
이소영 <th42500@naver.com>
2022-10-06 14:27:27 +0900

    파일의 내용 중 가장 앞글자(1byte) 출력하기

---
+++ java-exercise-1006/src/Pojo/ReadFile.java
@@ -0,0 +1,31 @@
+package Pojo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class ReadFile {
+
+    public static void main(String[] args) {
+        ReadFile readFile = new ReadFile();
+        char c = readFile.readAByte("./a_file.txt");
+        System.out.println(c);
+    }
+
+    public char readAByte(String fileName) {
+        char ch = 'a';
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            ch = br.readLine().charAt(0);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("읽어올 파일이 없습니다.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ch;
+    }
+
+}
