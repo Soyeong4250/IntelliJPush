6eca37cf3b8969dee332a4cb73fb608ed937b968
이소영 <th42500@naver.com>
2022-10-07 13:33:33 +0900

    Update  Source Code
    대용량 데이터에서 원하는 데이터만 추출하여 별도 파일로 저장하는 코드 추가

--- java-exercise-1007/src/PopulationStaticsMain.java
+++ java-exercise-1007/src/PopulationStaticsMain.java
@@ -1,4 +1,7 @@
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PopulationStaticsMain {
     public static void main(String[] args) throws IOException {
@@ -6,20 +9,46 @@ public class PopulationStaticsMain {
 
         // 1. 파일 읽어오기
         // 파일을 읽기 위한 준비
-        String path = "C:\\TECHIT\\git\\data\\2021_인구관련연간자료.csv";
+        //String path = "C:\\TECHIT\\git\\data\\2021_인구관련연간자료.csv";
 
         // 파일 읽기 (한 글자씩 총 100만 글자)
-        populationStatistics.readAChar(path);
+        /*populationStatistics.readAChar(path);
 
         // 파일 읽기 (한 줄씩 읽기)
         populationStatistics.readByLine(path);
 
         // 자바 8 스타일로 한 줄씩 읽기
-        populationStatistics.readByLine2(path);
+        populationStatistics.readByLine2(path);*/
 
-        /*String data = ""
-        PopulationMove pm = populationStatistics.parse(data);
-        System.out.println(pm.getFromSido());
-        System.out.println(pm.getToSido());*/
+
+        //List<PopulationMove> pml = populationStatistics.readByLine(path);
+
+        /*for (PopulationMove pm: pml) {
+            System.out.printf("전입:%s, 전출:%s\n", pm.getFromSido(), pm.getToSido());
+        }
+        System.out.println(pml.size());*/
+
+        // 4-1. 파일 생성
+        // populationStatistics.createAFile("from_to.txt");
+
+        // 4-2. 파일에 내용 작성
+        /*List<String> strings = new ArrayList<>();
+        for (PopulationMove pm: pml) {
+            // String으로 변환
+            String fromTo = populationStatistics.fromToString(pm);
+            strings.add(fromTo);
+        }
+
+        System.out.println(strings.size());  // 몇 건인지 확인 👉 6209323
+        populationStatistics.write(strings, "from_to.txt");*/
+
+        // 새로운 파일 읽어보기
+        String path = "./from_to.txt";
+        List<PopulationMove> pml = populationStatistics.readByLine(path);
+        for (PopulationMove pm: pml) {
+            System.out.printf("전입:%s, 전출:%s\n", pm.getFromSido(), pm.getToSido());
+        }
+        System.out.println(pml.size());
+
     }
 }
--- java-exercise-1007/src/PopulationStatistics.java
+++ java-exercise-1007/src/PopulationStatistics.java
@@ -1,9 +1,9 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PopulationStatistics {
     public void readAChar(String filename) throws IOException {
@@ -15,22 +15,24 @@ public class PopulationStatistics {
         System.out.println(fileContents);
     }
 
-    public void readByLine(String filename) throws IOException {
+    public List<PopulationMove> readByLine(String filename) throws IOException {
         // 파일 내용을 삽으로 푸듯이 크게 (한 줄씩) 읽어오기
         // 시도 1 : 공간 복잡도가 늘어남
         /*FileReader fileReader = new FileReader(filename);
         BufferedReader reader = new BufferedReader(fileReader);*/
 
         // 시도 1 Refactoring
+        List<PopulationMove> result = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(filename));
-
-
         String str;
 
         while((str = reader.readLine()) != null) {
-            System.out.println(str);
+            // System.out.println(str);
+            // PopulationMove pm = parse(str);
+            result.add(parse(str));
         }
         reader.close();
+        return result;
     }
 
     public void readByLine2(String filename) {
@@ -47,9 +49,43 @@ public class PopulationStatistics {
     }
 
     // 3-1. PopulationMove - parse() 메서드 구현
-    public  PopulationMove parse(String data) {
+    public PopulationMove parse(String data) {
         String[] input = data.split(",");
-        return new PopulationMove(input[0], input[6]);  // 전입, 전출 순서
+        // 이전 파일
+        //return new PopulationMove(input[0], input[6]);  // 전입, 전출 순서
+
+        // 새로운 파일
+        return new PopulationMove(input[0], input[1]);  // 전입, 전출
+    }
+
+    // 4. 필요한 데이터만 추출하여 파일로 만들기
+    // 4-1. 파일을 생성하는 메소드
+    public void createAFile(String filename) {
+        File file = new File(filename);
+        try {
+            file.createNewFile();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    // 4-2. 파일에 내용을 작성하는 메소드
+    public void write(List<String> strs, String filename) {
+        File file = new File(filename);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            for (String str : strs) {
+                writer.write(str);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 4-3. PopulationMove 객체의 데이터들을 String 형태로 변환하기
+    public String fromToString(PopulationMove populationMove) {
+        return populationMove.getFromSido() + "," +populationMove.getFromSido() + "\n";
     }
 
 
