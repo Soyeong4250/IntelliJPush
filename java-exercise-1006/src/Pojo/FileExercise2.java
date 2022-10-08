bd7f1dd5cd8a46a3da22cdef20d13e01ca7af277
이소영 <th42500@naver.com>
2022-10-08 17:11:09 +0900

    Add : 한 줄, 전체 내용 출력 메소드 추가

--- java-exercise-1006/src/Pojo/FileExercise2.java
+++ java-exercise-1006/src/Pojo/FileExercise2.java
@@ -1,7 +1,11 @@
 package Pojo;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileExercise2 {
     private String filename;
@@ -19,16 +23,45 @@ public class FileExercise2 {
             if(asciiCode == -1) {
                 return str;
             }
-            str += (char) fileReader.read();
+            str += (char)asciiCode;
         }
         return str;
     }
 
+    public String readALine() throws IOException {
+        FileReader fileReader = new FileReader(this.filename);
+        BufferedReader br = new BufferedReader(fileReader);
+        String str = br.readLine();
+
+        return str;
+    }
+
+    public void readAllLine() {
+        try(BufferedReader br = Files.newBufferedReader(
+                Paths.get(this.filename), StandardCharsets.UTF_8)) {
+
+            System.out.println("전체 문장 출력");
+            String str = "";
+            while(br.readLine() != null) {
+                str += br.readLine();
+            }
+            System.out.println(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         FileExercise2 fileExercise = new FileExercise2("./src/Pojo/a_file.txt");
-        String str = fileExercise.readNChars(5);
+        /*String str = fileExercise.readNChars(5);
         System.out.println("N 글자 출력");
         System.out.println(str);
+
+        String str1 = fileExercise.readALine();
+        System.out.println("한 개의 Line 출력");
+        System.out.println(str1);*/
+
+        fileExercise.readAllLine();
 
     }
 }
