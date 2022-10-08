26a92075390d83415196924605318c348ddf666e
이소영 <th42500@naver.com>
2022-10-08 17:09:49 +0900

    Add : 학생 객체 리스트 생성 및 출력

--- java-exercise-1006/src/List/ListExercise.java
+++ java-exercise-1006/src/List/ListExercise.java
@@ -6,18 +6,29 @@ import java.util.List;
 public class ListExercise {
 
     private List<String> students;
+    private List<Student> studentObjs;
 
     public ListExercise() {
         this.students = new ArrayList<>();
-        this.students.add("김경록");
-        this.students.add("김미지");
-        this.students.add("김민경");
-        this.students.add("김민지");
+        this.students.add("권하준");
+        this.students.add("조성윤");
+        this.students.add("안예은");
+        this.students.add("남우빈");
+        this.students.add("최경민");
+
+        this.studentObjs = new ArrayList<>();
+        this.studentObjs.add(new Student(1,"권하준","https://github.com/dongyeon-0822/java-project-exercise"));
+        this.studentObjs.add(new Student(1,"조성윤","https://github.com/kang-subin/Java"));
+        this.studentObjs.add(new Student(3,"안예은","https://github.com/KoKwanwun/LikeLion.git"));
+        this.studentObjs.add(new Student(1,"남우빈","https://github.com/lcomment/Algorithm_Solution--Java/tree/main/LikeLion"));
+        this.studentObjs.add(new Student(2,"최경민","https://github.com/cmkxak/likelion-java-course"));
     }
 
     public List<String> getStudents() {
         return students;
     }
-
+    public List<Student> getStudentObjs() {
+        return studentObjs;
+    }
 
 }
--- java-exercise-1006/src/List/ListExerciseMain.java
+++ java-exercise-1006/src/List/ListExerciseMain.java
@@ -7,13 +7,22 @@ public class ListExerciseMain {
     public static void main(String[] args) {
         ListExercise listExercise = new ListExercise();
 
-        List<String> students = listExercise.getStudents();
+        /*List<String> students = listExercise.getStudents();
 
+        System.out.println("List에 있는 학생 이름을 출력합니다.");
         for (String student : students) {
             System.out.println(student);
         }
 
-        System.out.println("총 인원은 " + students.size());
+        System.out.println("총 인원은 " + students.size()); */
+
+
+       List<Student> studentObjs = listExercise.getStudentObjs();
+        System.out.println("List에 있는 학생 정보를 출력합니다.");
+        for (Student student : studentObjs) {
+            System.out.println(student.toString());
+        }
+
     }
 
 }
