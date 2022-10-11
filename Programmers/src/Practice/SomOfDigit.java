d5bcb42fb0fdd6d39cbaf2e889190de1253d5192
이소영 <th42500@naver.com>
2022-10-11 10:03:59 +0900

    Add SomOfDigit.java
    프로그래머스 자릿수 더하기 다른 풀이 추가

--- Programmers/src/Practice/SomOfDigit.java
+++ Programmers/src/Practice/SomOfDigit.java
@@ -2,13 +2,27 @@ package Practice;
 
 public class SomOfDigit {
     private int solution(int n) {
+
         int answer = 0;
         while(n > 0) {
             answer += n%10;
             n /= 10;
         }
+
         return answer;
     }
+
+    /*private int solution(int n) {
+        // n을 String으로 변경
+        String str = String.valueOf(n);
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            result += Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+
+        // String을 loop 돌면서 parseInt 후 더함
+        return result;
+    }*/
 
     public static void main(String[] args) {
         SomOfDigit sod = new SomOfDigit();
