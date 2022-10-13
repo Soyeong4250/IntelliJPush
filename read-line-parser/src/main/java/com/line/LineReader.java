c56e4438ba3c8f47a35195e320759e798edde97a
이소영 <th42500@naver.com>
2022-10-13 15:37:29 +0900

    Refactor Parsing Code
    1. Parsing이 잘 되는지 눈으로 확인할 수 있도록 Main 클래스 수정
    2. 한글 깨짐 해결을 위해 LineReader 클래스 수정

--- read-line-parser/src/main/java/com/line/LineReader.java
+++ read-line-parser/src/main/java/com/line/LineReader.java
@@ -2,9 +2,7 @@ package com.line;
 
 import com.line.parser.Parser;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -27,11 +25,12 @@ public class LineReader<T> {
     public List<T> readLines(String filename) throws IOException {
         List<T> result = new ArrayList<>();
 
-        // BufferedReader br = new BufferedReader(new FileReader(filename));
+//        BufferedReader br = new BufferedReader(new FileReader(filename));
         BufferedReader br;
         br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8);
         String str;
-        br.readLine();  // 첫 한줄 (제목 행) 날리기
+        String temp = br.readLine();  // 첫 한줄 (제목 행) 날리기
+        System.out.println(temp);
         while((str = br.readLine()) != null) {
             result.add(parser.parse(str));
         }
