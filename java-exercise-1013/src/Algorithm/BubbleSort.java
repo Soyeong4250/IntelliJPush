cd250929db9c381f9edd8c148bcf8fe802a3e181
이소영 <th42500@naver.com>
2022-10-13 09:29:29 +0900

    Add BubbleSort.java
    1. Bubble정렬을 한 int 형 배열을 반환하는 sort() 메소드 구현

--- java-exercise-1013/src/Algorithm/BubbleSort.java
+++ java-exercise-1013/src/Algorithm/BubbleSort.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     private int[] sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             for (int j = i+1; j < arr.length; j++) {
                 if(arr[i] > arr[j]) {
                     int temp = arr[i];
