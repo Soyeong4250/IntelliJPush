ca7b731fa3a160e3d094bef26f274920873c8786
이소영 <th42500@naver.com>
2022-10-13 09:26:59 +0900

    Add BubbleSort.java
    1. Bubble정렬을 한 int 형 배열을 반환하는 sort() 메소드 구현

---
+++ java-exercise-1013/src/Algorithm/BubbleSort.java
@@ -0,0 +1,33 @@
+package Algorithm;
+
+/*
+문제 : {7, 2, 3, 9, 28, 11}에서 가장 작은 숫자가 0번 자리에 오도록 하는 알고리즘 구현
+ */
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    private int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort bubble = new BubbleSort();
+
+        int[] arr = {7, 2, 3, 9, 28, 11};
+        int[] result = bubble.sort(arr);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+}
