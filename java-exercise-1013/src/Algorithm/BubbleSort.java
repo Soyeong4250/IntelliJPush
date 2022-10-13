b570cab8abc31a14d2790f400af45351a912c9cc
이소영 <th42500@naver.com>
2022-10-13 10:09:59 +0900

    Refactoring BubbleSort.java
    1. 2중 for문을 활용한 BubbleSort 정렬 구현

--- java-exercise-1013/src/Algorithm/BubbleSort.java
+++ java-exercise-1013/src/Algorithm/BubbleSort.java
@@ -7,15 +7,17 @@ package Algorithm;
 import java.util.Arrays;
 
 public class BubbleSort {
-    private int[] sort(int[] arr, int i) {
-        for (int j = i+1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    private int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
 
-            System.out.println(Arrays.toString(arr));
+                System.out.println(Arrays.toString(arr));
+            }
         }
 
         return arr;
@@ -25,7 +27,7 @@ public class BubbleSort {
         BubbleSort bubble = new BubbleSort();
 
         // int[] arr = {7, 2, 3, 9, 28, 11};
-        int[] result = bubble.sort(new int[]{7, 2, 3, 9, 28, 11}, 0);
+        int[] result = bubble.sort(new int[]{7, 2, 3, 9, 28, 11});
 
         System.out.println(Arrays.toString(result));
     }
