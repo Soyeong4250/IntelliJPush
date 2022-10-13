d3243e66a080d9b9f7122f59b92cc679039a6433
이소영 <th42500@naver.com>
2022-10-13 09:40:04 +0900

    Add BubbleSort.java
    1. 배열의 0번째 인덱스 값과 전체 배열을 비교하여 수 정렬하기

--- java-exercise-1013/src/Algorithm/BubbleSort.java
+++ java-exercise-1013/src/Algorithm/BubbleSort.java
@@ -8,13 +8,11 @@ import java.util.Arrays;
 
 public class BubbleSort {
     private int[] sort(int[] arr) {
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[0] > arr[i]) {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
             }
         }
 
