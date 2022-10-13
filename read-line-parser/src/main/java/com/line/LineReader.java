9150795c692db87f84c77f2ced141903e568c011
이소영 <th42500@naver.com>
2022-10-13 14:41:55 +0900

    첫번째 데이터를 읽어 원하는 데이터만 추출하기 & 단위테스트 실행

--- read-line-parser/src/main/java/com/line/LineReader.java
+++ read-line-parser/src/main/java/com/line/LineReader.java
@@ -5,6 +5,9 @@ import com.line.parser.Parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +27,9 @@ public class LineReader<T> {
     public List<T> readLines(String filename) throws IOException {
         List<T> result = new ArrayList<>();
 
-        BufferedReader br = new BufferedReader(new FileReader(filename));
+        // BufferedReader br = new BufferedReader(new FileReader(filename));
+        BufferedReader br;
+        br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8);
         String str;
         br.readLine();  // 첫 한줄 (제목 행) 날리기
         while((str = br.readLine()) != null) {
--- read-line-parser/src/main/java/com/line/domain/Hospital.java
+++ read-line-parser/src/main/java/com/line/domain/Hospital.java
@@ -1,19 +1,73 @@
 package com.line.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Hospital {
     private String id;
     private String address;
     private String district;
     private String category;
-    private String emergencyRoom;  // Java는 camel, Python은 snake
+    private int emergencyRoom;  // Java는 camel, Python은 snake
     private String name;
     private String subdivision;
 
-    public Hospital(String id) {
+
+    public Hospital(String id, String address, String category, int emergencyRoom, String name) {
         this.id = id;
+        this.address = address;
+        this.category = category;
+        this.emergencyRoom = emergencyRoom;
+        this.name = name;
+        this.setDistrict();
+        this.setSubdivision();
     }
+
 
     public String getId() {
         return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public String getDistrict() {
+//        String[] splitted = this.address.split(" ");
+//        return String.format("%s %s", splitted[0], splitted[1]);
+        return district;
+    }
+    public void setDistrict() {
+        String[] splitted = this.address.split(" ");
+        this.district = String.format("%s %s", splitted[0], splitted[1]);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getEmergencyRoom() {
+        return emergencyRoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
+    }
+
+    private void setSubdivision() {
+        String[] subdivisionList = {"내과", "치과", "피부과", "성형외과", "외과", "소아과", "가정의학과", "의원"};
+        String subdivision = this.name;
+        for (String division:subdivisionList) {
+            if(subdivision.contains(division)) {
+                subdivision = division;
+                break;
+            }
+        }
+        this.subdivision = subdivision;
     }
 }
