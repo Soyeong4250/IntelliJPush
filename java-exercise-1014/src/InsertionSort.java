4d8a3d3ff206bfa8c013e19388528ce429c62600
이소영 <th42500@naver.com>
2022-10-14 10:17:45 +0900

    Refactor InsertionSort.java
    1. 두 번째 단계 구현해보며 Loop 떠올려보기

--- java-exercise-1014/src/InsertionSort.java
+++ java-exercise-1014/src/InsertionSort.java
@@ -1,14 +1,16 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-    private int[] sort(int[] arr) {
-        int i =1;
-
-        if(arr[i] < arr[i-1]) {  // arr[1] vs arr[0]
+    private int[] sort(int[] arr, int i) {
+        // i = 1 arr[1] j vs arr[0]
+        // i = 2 일때 arr[2] j vs arr[1]
+        // i = 2 일때 arr[1] j vs arr[0]
+        int j = i;
+        if(arr[j] < arr[j-1]) {
             // swap
-            int temp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = temp;
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
         }
 
         return arr;
@@ -17,9 +19,11 @@ public class InsertionSort {
     public static void main(String[] args) {
         // 5에서 출발을 합니다. idx = 1
         int[] arr = {8, 5, 6, 2, 4};
+        // 첫번째 정렬 후
+        arr = new int[]{5, 8, 6, 2, 4};
 
         InsertionSort insert = new InsertionSort();
-        int[] result = insert.sort(arr);
+        int[] result = insert.sort(arr, 1);
         System.out.println(Arrays.toString(result));
     }
 
