0f5d0424d32e3ff4d7269e9579701514c92cfbdc
이소영 <th42500@naver.com>
2022-10-14 10:36:51 +0900

    Refactor InsertionSort.java
    1. 재귀적 용법으로 삽입정렬 구현

--- java-exercise-1014/src/InsertionSort.java
+++ java-exercise-1014/src/InsertionSort.java
@@ -1,32 +1,33 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-    private int[] sort(int[] arr) {
+    private int[] sort(int[] arr, int i) {
         // i = 1 arr[1] j vs arr[0]
         // i = 2 일때 arr[2] j vs arr[1]
         // i = 2 일때 arr[1] j vs arr[0]
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j > 0; j--) {
-                System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
-                if(arr[j] < arr[j-1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                }
+        if(i == arr.length) return arr;
+
+        for (int j = i; j > 0; j--) {
+            // System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
+
+            if(arr[j] < arr[j-1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
             }
         }
-        return arr;
+
+        return sort(arr, i+1);
     }
 
     public static void main(String[] args) {
         // 5에서 출발을 합니다. idx = 1
         int[] arr = {8, 5, 6, 2, 4};
         // 첫번째 정렬 후
-        arr = new int[]{5, 8, 6, 2, 4};
+        // arr = new int[]{5, 8, 6, 2, 4};
 
         InsertionSort insert = new InsertionSort();
-        int[] result = insert.sort(arr);
+        int[] result = insert.sort(arr, 1);
         System.out.println(Arrays.toString(result));
     }
 
