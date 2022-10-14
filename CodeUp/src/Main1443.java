a1a7801e77cf22356ffd3576856bbe1512315f37
이소영 <th42500@naver.com>
2022-10-14 10:45:37 +0900

    Refactor Main1443.java
    1. CodeUp 삽입 정렬 재귀로 구현

--- CodeUp/src/Main1443.java
+++ CodeUp/src/Main1443.java
@@ -2,18 +2,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main1443 {  // 삽입 정렬
-    private int[] sort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if(arr[j] < arr[j-1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                }
+    private int[] sort(int[] arr, int i) {
+        if(i == arr.length) {
+            return arr;
+        }
+
+        for (int j = i; j > 0; j--) {
+            if(arr[j] < arr[j-1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
             }
         }
 
-        return arr;
+        return sort(arr, i+1);
     }
 
     public static void main(String[] args) {
@@ -27,7 +29,7 @@ public class Main1443 {  // 삽입 정렬
             arr[i] = sc.nextInt();
         }
 
-        int[] result = main.sort(arr);
+        int[] result = main.sort(arr, 1);
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
