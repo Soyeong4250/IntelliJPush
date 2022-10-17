fe7dfc285d9eeecb8c39278efc0c56fd1d5e625b
이소영 <th42500@naver.com>
2022-10-17 10:31:18 +0900

    Refactor Diamond.java
    1. 홀수개 마름모 별찍기

--- java-exercise-1017/src/Diamond.java
+++ java-exercise-1017/src/Diamond.java
@@ -39,12 +39,45 @@ public class Diamond {
         }
 
     }
+    private void printStart2(int n) {
+        /*
+           *
+          ***
+         *****
+          ***
+           *
+        */
+
+        // 마름모 모양으로 별 찍기 (홀수)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*i+1; j++) {
+                System.out.print(this.letter);
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*(n-i)-1; j++) {
+                System.out.print(this.letter);
+            }
+            System.out.println();
+        }
+
+    }
 
     public static void main(String[] args) {
         Diamond diamond = new Diamond();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         diamond.printStar(n);
+        System.out.println("------------------");
+        diamond.printStart2(n);
     }
+
 
 }
