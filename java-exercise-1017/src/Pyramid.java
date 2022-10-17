b420dc8568887a50c343bc06b7a377e557c71211
이소영 <th42500@naver.com>
2022-10-17 10:12:27 +0900

    Add Pyramid.java
    1. 피라미드 모양으로 별찍기

---
+++ java-exercise-1017/src/Pyramid.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    private String letter = "*";
+
+    public Pyramid() {
+    }
+
+    public Pyramid(String letter) {
+        this.letter = letter;
+    }
+
+    private void printStar(int n) {
+        /*
+             *
+            * *
+           * * *
+          * * * *
+         * * * * *
+         */
+        //  피라미드 모양으로 별 찍기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(this.letter + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Pyramid pyramid = new Pyramid();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pyramid.printStar(n);
+    }
+
+}
