46048adff0111ac346d48c8f9cfa653bf84e4bee
이소영 <th42500@naver.com>
2022-10-18 11:03:44 +0900

    Refactor subdivision 수정 및 toString() 추가

--- read-line-parser/src/main/java/com/line/domain/Hospital.java
+++ read-line-parser/src/main/java/com/line/domain/Hospital.java
@@ -3,7 +3,6 @@ package com.line.domain;
 import java.util.Arrays;
 import java.util.List;
 
-// 5. Hospital 객체 생성 (멤버변수, 생성자(처음에는 모든 멤버변수를 매개변수에 넣어서 생성), getter) 👉 다시 HospitalParse 이동
 public class Hospital {
     private String id;
     private String address;
@@ -13,9 +12,6 @@ public class Hospital {
     private String name;
     private String subdivision;
 
-
-    // 8. HospitalParser에서 넘겨준 데이터에 맞게 매개변수 수정하고 setter 이용하여 데이터 저장하기
-    // 👉 Main 클래스로 이동하자
     public Hospital(String id, String address, String category, int emergencyRoom, String name) {
         this.id = id;
         this.address = address;
@@ -63,17 +59,15 @@ public class Hospital {
     }
 
     private void setSubdivision() {
-        String[] subdivisionList = {"소아", "피부", "성형", "정형외과", "척추", "교정", "산부인과", "관절", "봉합", "화상", "골절", "영유아", "안과", "가정의학과", "비뇨기과", "치과", "한방", "요양", "의료시설", "보건", "내과", "외과", "병원", "의원"};
+        String[] subdivisionList = {"소아", "피부", "성형", "정형외과", "척추", "교정", "산부인과", "관절", "봉합", "화상", "골절", "영유아", "안과", "가정의학과", "비뇨기과", "치과", "한방", "요양", "보건", "내과", "외과", "병원", "의원", };
         String subdivision = this.name;
         for (String division:subdivisionList) {
             if(subdivision.contains("노인")) {
                 subdivision = "요양";
                 break;
-            }else if(subdivision.contains("재단") || subdivision.contains("센터")) {
-                subdivision = "의료시설";
-                break;
             }else if(subdivision.contains("조산")) {
                 subdivision = "산부인과";
+                break;
             }else if(subdivision.contains(division)) {
                 subdivision = division;
                 break;
@@ -82,7 +76,6 @@ public class Hospital {
         this.subdivision = subdivision;
     }
 
-    // 11. SQL문 생성 코드를 작성
     public String getSqlInsertQuery() {
         String sql = String.format("INSERT IGNORE INTO `test-db`.`seoul_hospital`\n" +
                 "(`id`,`address`,`district`,`category`,`emergency_room`,`name`,`subdivision`)\n"+
@@ -97,9 +90,6 @@ public class Hospital {
         return sql;
     }
 
-    // 11-1. getSqlInsertQuery()만 이용하여 sql문을 작성하면 한 줄의 데이터마다 새로운 insert문을 생성함
-    // 👉 나중에 workbench에 파일을 넣어데이터를 생성하였을 때 속도가 매우 느림
-    // 이를 위해 insert문을 한번에 만들기 위한 메서드를 생성 👉 다시 Main으로 이동
     public String getTupleString() {
         String tuple = String.format("\n(\"%s\",\n" +
                         "\"%s\",\n" +
@@ -110,5 +100,18 @@ public class Hospital {
                         "\"%s\")", this.id, this.address, this.district, this.category, this.emergencyRoom, this.name, this.subdivision);
 
         return tuple;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospital{" +
+                "id='" + id + '\'' +
+                ", address='" + address + '\'' +
+                ", district='" + district + '\'' +
+                ", category='" + category + '\'' +
+                ", emergencyRoom=" + emergencyRoom +
+                ", name='" + name + '\'' +
+                ", subdivision='" + subdivision + '\'' +
+                '}';
     }
 }
