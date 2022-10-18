d162a7e1eb330ded9a67687e437589e2d805353e
이소영 <th42500@naver.com>
2022-10-18 14:34:41 +0900

    Refactor Class로 분리

--- read-line-parser/src/main/java/com/line/dao/HospitalDao.java
+++ read-line-parser/src/main/java/com/line/dao/HospitalDao.java
@@ -6,20 +6,30 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class HospitalDao {
-    public void selectAll() throws SQLException {
+
+    private AWSConnectionMaker awsConnectionMaker;
+
+    public HospitalDao() {
+        this.awsConnectionMaker = new AWSConnectionMaker();
+    }
+
+    public List<Hospital> selectAll() throws SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
+        List<Hospital> hospitalList = null;
 
         try {
-            conn = ConnectionMaker.getConnection();  // db 연결
+            conn = awsConnectionMaker.getConnection();  // db 연결
             ps = conn.prepareStatement("SELECT id, address, district, category, emergency_room, name, subdivision FROM seoul_hospital");
 
             rs = ps.executeQuery();
             while(rs.next()) {
                 Hospital hospital = new Hospital(rs.getString("id"), rs.getString("address"), rs.getString("category"), rs.getInt("emergency_room"), rs.getString("name"));
+                hospitalList.add(hospital);
                 System.out.println(hospital.toString());
             }
         } catch (SQLException e) {
@@ -27,21 +37,23 @@ public class HospitalDao {
         } finally {
             ConnectionClose.close(conn, ps, rs);
         }
+        return hospitalList;
     }
 
-    public void selectById(String sId) throws SQLException {
+    public String selectById(String sId) throws SQLException {
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
+        Hospital hospital = null;
 
         try {
-            conn = ConnectionMaker.getConnection();  // db 연결
+            conn = awsConnectionMaker.getConnection();  // db 연결
             ps = conn.prepareStatement("SELECT id, address, district, category, emergency_room, name, subdivision FROM seoul_hospital WHERE id = ?");
             ps.setString(1, sId);
 
             rs = ps.executeQuery();
             if(rs.next()) {
-                Hospital hospital = new Hospital(rs.getString("id"), rs.getString("address"), rs.getString("category"), rs.getInt("emergency_room"), rs.getString("name"));
+                hospital = new Hospital(rs.getString("id"), rs.getString("address"), rs.getString("category"), rs.getInt("emergency_room"), rs.getString("name"));
                 System.out.println(hospital.toString());
             }
         } catch (SQLException e) {
@@ -49,15 +61,17 @@ public class HospitalDao {
         } finally {
             ConnectionClose.close(conn, ps, rs);
         }
+        return hospital.toString();
     }
 
-    public void insertData(Hospital hospital) {
+    public int insertData(Hospital hospital) {
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
+        int status = 0;
 
         try {
-            conn = ConnectionMaker.getConnection();// db 연결
+            conn = awsConnectionMaker.getConnection();// db 연결
             ps = conn.prepareStatement("INSERT IGNORE INTO seoul_hospital(id, address, district, category, emergency_room, name, subdivision) VALUES(?, ?, ?, ?, ?, ?, ?)");
             ps.setString(1, hospital.getId());
             ps.setString(2, hospital.getAddress());
@@ -67,7 +81,7 @@ public class HospitalDao {
             ps.setString(6, hospital.getName());
             ps.setString(7, hospital.getSubdivision());
 
-            int status = ps.executeUpdate();// ctrl + enter
+            status = ps.executeUpdate();// ctrl + enter
             if(status == 1) {
                 System.out.println("데이터 추가 성공");
             }
@@ -76,5 +90,6 @@ public class HospitalDao {
         } finally {
             ConnectionClose.close(conn, ps);
         }
+        return status;
     }
 }
