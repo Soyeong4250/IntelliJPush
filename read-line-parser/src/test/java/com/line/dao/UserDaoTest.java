bc729ed6b56713b7263016e5119a1a78e7c66e23
이소영 <th42500@naver.com>
2022-10-19 11:51:01 +0900

    Test UserDaoTest insert, select 기능 테스트 구현

---
+++ read-line-parser/src/test/java/com/line/dao/UserDaoTest.java
@@ -0,0 +1,31 @@
+package com.line.dao;
+
+import com.line.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    @DisplayName("UserInsert 테스트")
+    void addTest() throws SQLException {
+        UserDao userDao = new UserDao();
+        User user = new User("1", "Soyeong", "1234");
+        Assertions.assertEquals(userDao.insertData(user), 1);
+    }
+
+    @Test
+    @DisplayName("userSelect 테스트")
+    void searchByIdTest() throws SQLException {
+        String sId = "1";
+        UserDao userDao = new UserDao();
+        String result = userDao.selectById(sId);
+        Assertions.assertEquals(result, "1 Soyeong 1234");
+    }
+
+}
