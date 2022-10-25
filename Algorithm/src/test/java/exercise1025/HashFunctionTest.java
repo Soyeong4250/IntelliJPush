008327e080a70e4aaf4c812715887a2c1f4d0cc8
이소영 <th42500@naver.com>
2022-10-25 09:22:24 +0900

    [Add] Hash 구현

---
+++ Algorithm/src/test/java/exercise1025/HashFunctionTest.java
@@ -0,0 +1,25 @@
+package exercise1025;
+
+import com.likelion.practice.exercise1025.HashFunction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HashFunctionTest {
+    HashFunction hashFunction;
+
+    @BeforeEach
+    void setUp() {
+        hashFunction = new HashFunction();
+
+
+    }
+
+    @Test
+    @DisplayName("")
+    void solution() {
+        assertEquals(53, hashFunction.hash("Kyeongrok"));
+    }
+}
