9f85c2f5781b918e8b41d56831d4ef7b7a3ee312
이소영 <th42500@naver.com>
2022-10-26 10:16:37 +0900

    [Add] hash() 구현

---
+++ Algorithm/src/main/java/com/likelion/practice/exercise1026/HashTable1.java
@@ -0,0 +1,39 @@
+package com.likelion.practice.exercise1026;
+
+import com.likelion.practice.exercise1025.HashTable;
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class HashTable1 {
+
+    List<Node>[] table = new ArrayList[1000];
+
+    public int hash(String str) {
+        int asciiSum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            asciiSum += str.charAt(i);
+        }
+        return asciiSum % 1000;
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"DongyeonKang",
+                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
+
+                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SoyeongLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
+                "JunhaHwang"};
+
+        HashTable1 hashTable = new HashTable1();
+        Set<Integer> nameSet = new HashSet<>();
+        for (int i = 0; i < names.length; i++) {
+            nameSet.add(hashTable.hash(names[i]));
+        }
+        System.out.printf("%s %s", names.length, nameSet.size());
+
+    }
+}
