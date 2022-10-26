9ec20f21098e881eb05ed62f4a625c471e0a8dcb
이소영 <th42500@naver.com>
2022-10-26 18:43:45 +0900

    [Add] @PathVariable 실습

--- hello/hello/src/main/java/com/springboot/hello/controller/HelloController.java
+++ hello/hello/src/main/java/com/springboot/hello/controller/HelloController.java
@@ -18,4 +18,15 @@ public class HelloController {
         return "Soyeong";
     }
 
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
+    }
+
+    
 }
