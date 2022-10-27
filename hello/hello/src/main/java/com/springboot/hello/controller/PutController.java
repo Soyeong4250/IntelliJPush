6cd3344b3b129c8f206fd6045f4ed697ea0d1de1
이소영 <th42500@naver.com>
2022-10-27 15:20:43 +0900

    [Add] PUT API 추가

---
+++ hello/hello/src/main/java/com/springboot/hello/controller/PutController.java
@@ -0,0 +1,30 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+    @PutMapping(value = "/member")
+    public String putMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry map : putData.entrySet()) {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        }
+
+        System.out.println("request가 호출 완료되었습니다.");
+
+        return sb.toString();
+    }
+
+    @PutMapping(value = "/member2")
+    public String putMemberDto(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    
+}
