572080bb7e8c95bc7fd0f027964bd96a4068418c
이소영 <th42500@naver.com>
2022-10-27 10:40:30 +0900

    [Fix] 프로그래머스 완주하지 못한 선수 중복처리

--- Algorithm/src/main/java/com/likelion/programmers/Marathon1.java
+++ Algorithm/src/main/java/com/likelion/programmers/Marathon1.java
@@ -10,20 +10,23 @@ public class Marathon1 {
 
         for (int i = 0; i < participant.length; i++) {
             String key = participant[i];
-            memo.put(key, 1);
+            if(!memo.containsKey(key)) {
+                memo.put(key, 0);
+            }
+            memo.put(key, memo.get(key) + 1);
         }
 
         for (int i = 0; i < completion.length; i++) {
             String key = completion[i];
-            memo.put(key, 0);
+            memo.put(key, memo.get(key) - 1);
         }
-
-        for (String key : memo.keySet()) {
-            if(memo.get(key) == 1) {
-                return key;
+        String answer = "";
+        for (String key:memo.keySet()) {
+            if(memo.get(key) != 0) {
+                answer = key;
             }
         }
-        return "";
+        return answer;
     }
     
     public static void main(String[] args) {
