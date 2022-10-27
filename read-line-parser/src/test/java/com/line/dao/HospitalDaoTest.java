38ea65582e9235164ac598ed03bc6c1b2cc8c06f
이소영 <th42500@naver.com>
2022-10-27 11:43:10 +0900

    [Test] Jdbc Template 적용으로 인한 코드 수정

--- read-line-parser/src/test/java/com/line/dao/HospitalDaoTest.java
+++ read-line-parser/src/test/java/com/line/dao/HospitalDaoTest.java
@@ -1,30 +1,91 @@
 package com.line.dao;
 
 import com.line.domain.Hospital;
+import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = HospitalDaoFactory.class)
 class HospitalDaoTest {
-    @Test
-    @DisplayName("insert 테스트")
-    void addTest() {
-        HospitalDao hospitalDao = new HospitalDao();
-        Hospital hospital = new Hospital("Testdata", "서울특별시 종로구 종로3길 17 D타워, 16-17층", "T", 0, "멋사소아과의원");
-        Assertions.assertEquals(hospitalDao.insertData(hospital), 1);
+
+    @Autowired
+    ApplicationContext context;
+    HospitalDao hospitalDao;
+
+    /*@BeforeEach
+    void setUp() throws SQLException {
+        this.hospitalDao = context.getBean("awsHospitalDao", HospitalDao.class);
+        HospitalDao.deleteAll();
+        HospitalDao.add(new User("0", "Soyeong", "0000"));
+        System.out.println("Before Each");
     }
 
     @Test
-    @DisplayName("select 테스트")
-    void searchByIdTest() throws SQLException {
-        String sId = "Testdata";
-        HospitalDao hospitalDao = new HospitalDao();
-        String result = hospitalDao.selectById(sId);
-        Assertions.assertEquals(result, "Hospital{id='Testdata', address='서울특별시 종로구 종로3길 17 D타워, 16-17층', district='서울특별시 종로구', category='T', emergencyRoom=0, name='멋사소아과의원', subdivision='소아'}");
+    @DisplayName("User가 null인 경우")
+    void findById() {
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            userDao.selectById("100");
+        });
     }
+
+    @Test
+    @DisplayName("add, select 테스트")
+    void addAndSelect() throws SQLException {
+        assertEquals(1, userDao.getCount());
+
+        User user1 = new User("1", "kyeonghwan", "1123");
+        userDao.add(user1);
+        assertEquals(2, userDao.getCount());
+        User user = userDao.selectById(user1.getId());
+
+        assertEquals(user1.getName(), user.getName());
+        assertEquals(user1.getPassword(), user.getPassword());
+    }
+
+    @Test
+    @DisplayName("getCount 테스트")
+    void getCountTest() throws SQLException {
+        int cnt = userDao.getCount();
+        assertEquals(1, cnt);
+        userDao.add(new User("1", "kyeonghwan", "1234"));
+        assertEquals(2, userDao.getCount());
+        userDao.add(new User("2", "sujin", "4321"));
+        assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    @DisplayName("deleteAll 테스트")
+    void deleteTest() throws SQLException {
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+    }
+
+    @Test
+    @DisplayName("selectAll 테스트")
+    void selectAllTest() throws SQLException {
+        userDao.deleteAll();
+        List<User> userList = userDao.selectAll();
+        assertEquals(0, userList.size());
+        User user1 = new User("1", "kyeonghwan", "1123");
+        userDao.add(user1);
+        User user2 = new User("2", "sujin", "4321");
+        userDao.add(user2);
+        userList = userDao.selectAll();
+        assertEquals(2, userList.size());
+    }*/
 
 }
