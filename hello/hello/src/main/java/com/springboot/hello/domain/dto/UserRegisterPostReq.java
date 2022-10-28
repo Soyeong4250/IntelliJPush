c4ae4d39d4cf4bf489ea0d92d7430eff6d94b491
이소영 <th42500@naver.com>
2022-10-28 14:50:10 +0900

    [Add] User 등록을 위한 Request 클래스 추가

---
+++ hello/hello/src/main/java/com/springboot/hello/domain/dto/UserRegisterPostReq.java
@@ -0,0 +1,40 @@
+package com.springboot.hello.domain.dto;
+
+public class UserRegisterPostReq {
+    private String id;
+    private String name;
+    private String password;
+
+    public UserRegisterPostReq() {
+    }
+
+    public UserRegisterPostReq(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
