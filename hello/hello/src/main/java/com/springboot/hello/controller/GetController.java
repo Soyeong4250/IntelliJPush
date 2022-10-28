f9616395ccb1768881d1f3b5ee6522b28020cf7b
이소영 <th42500@naver.com>
2022-10-28 11:39:13 +0900

    [Add] GetController에 로깅 레벨 설정

--- hello/hello/src/main/java/com/springboot/hello/controller/GetController.java
+++ hello/hello/src/main/java/com/springboot/hello/controller/GetController.java
@@ -3,12 +3,14 @@ package com.springboot.hello.controller;
 import com.springboot.hello.domain.dto.MemberDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     // @RequestMapping("/hello")
@@ -25,11 +27,13 @@ public class GetController {
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1으로 요청이 들어왔습니다. variable: { }", variable);
         return variable;
     }
 
     @GetMapping(value = "/variable2/{variable}")
     public String getVariable2(@PathVariable("variable") String var) {
+        log.info("variable이 호출 완료되었습니다.");
         return var;
     }
 
@@ -38,6 +42,7 @@ public class GetController {
     public String getRequestParam1 (@ApiParam(value = "이름", required = true) @RequestParam String name,
                                     @ApiParam(value = "이메일", required = true) @RequestParam String email,
                                     @ApiParam(value = "회사", required = true) @RequestParam String organization){
+        log.info("request1이 호출 완료되었습니다.");
         return name + " " + email + " " + organization;
     }
 
@@ -53,7 +58,7 @@ public class GetController {
             sb.append(map.getKey() + " : " + map.getValue() + "\n");
         });
 
-        System.out.println("request가 호출 완료되었습니다.");
+        log.info("request2가 호출 완료되었습니다.");
 
         return sb.toString();
     }
