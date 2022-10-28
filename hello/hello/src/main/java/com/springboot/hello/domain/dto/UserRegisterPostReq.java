cc956540a114713beb0e6ce6504c4487deeca47a
이소영 <th42500@naver.com>
2022-10-28 15:12:39 +0900

    [Refactor] UserRegisterPostReq에 Lombok 적용

--- hello/hello/src/main/java/com/springboot/hello/domain/dto/UserRegisterPostReq.java
+++ hello/hello/src/main/java/com/springboot/hello/domain/dto/UserRegisterPostReq.java
@@ -1,40 +1,12 @@
 package com.springboot.hello.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class UserRegisterPostReq {
     private String id;
     private String name;
     private String password;
-
-    public UserRegisterPostReq() {
-    }
-
-    public UserRegisterPostReq(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
