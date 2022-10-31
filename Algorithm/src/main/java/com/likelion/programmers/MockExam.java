36640b05e904c806972ec8874819f4a39e50a7a0
이소영 <th42500@naver.com>
2022-10-31 09:54:38 +0900

    [Add] 가장 많이 맞춘 수포자 배열을 return하기 위한 코드

--- Algorithm/src/main/java/com/likelion/programmers/MockExam.java
+++ Algorithm/src/main/java/com/likelion/programmers/MockExam.java
@@ -1,6 +1,8 @@
 package com.likelion.programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MockExam {
     private int[] solution(int[] answers) {
@@ -11,18 +13,31 @@ public class MockExam {
         4. Math.max() 또는 for문을 이용하여 가장 많이 맞힌 수포자를 구한다.
          */
 
-        int[] result = new int[3];
+        // 수포자는 1번부터 시작하므로 배열 길이 +1
+        int[] score = new int[4];
 
         int[] student1 = {1, 2, 3, 4, 5};
         int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
         for (int i = 0; i < answers.length; i++) {
-            if(answers[i] == student1[i]) result[0]+=1;
-            if(answers[i] == student2[i]) result[0]+=1;
-            if(answers[i] == student3[i]) result[0]+=1;
+            if(answers[i] == student1[i]) score[1]+=1;
+            if(answers[i] == student2[i]) score[2]+=1;
+            if(answers[i] == student3[i]) score[3]+=1;
         }
 
+        // 배열로 선언 시 크기 동적 할당❌ 👉 List에 담았다가 배열에 다시 담아주기
+        List<Integer> list = new ArrayList<>();
+        int max = Math.max(Math.max(score[0], score[1]), score[2]);
+        for (int i = 1; i < score.length; i++) {
+            if(max == score[i]) {
+                list.add(i+1);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
         return result;
     }
 
