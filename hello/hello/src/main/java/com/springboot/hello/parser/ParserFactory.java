3f3e7d51d7adf93789135460f5868fd46beab634
이소영 <th42500@naver.com>
2022-11-01 09:58:18 +0900

    [Fix] ParserFactory에 @Configure 어노테이션 추가

--- hello/hello/src/main/java/com/springboot/hello/parser/ParserFactory.java
+++ hello/hello/src/main/java/com/springboot/hello/parser/ParserFactory.java
@@ -3,7 +3,9 @@ package com.springboot.hello.parser;
 import com.springboot.hello.domain.Hospital;
 import com.springboot.hello.domain.dao.HospitalDao;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ParserFactory {
 
     @Bean
