1f35eceb1ec9735cb1545d5c1b198377bb509c4f
이소영 <th42500@naver.com>
2022-11-01 13:29:50 +0900

    [Add] HospitalDao insert() 기능을 위한 add() 추가

--- hello/hello/src/main/java/com/springboot/hello/domain/dao/HospitalDao.java
+++ hello/hello/src/main/java/com/springboot/hello/domain/dao/HospitalDao.java
@@ -1,5 +1,6 @@
 package com.springboot.hello.domain.dao;
 
+import com.springboot.hello.domain.Hospital;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -13,4 +14,17 @@ public class HospitalDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // 11만건의 데이터를 List<Hospital>에서 Hospital로 꺼내어 insert()
+    public void add(Hospital hospital) {
+        String sql = "insert into `testdb`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`,\n" +
+                "`management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`,\n" +
+                "`business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`,\n" +
+                "`total_area_size`) " +
+                "values (?, ?, ?, ?, ?, ?, ?,\n" +
+                "?, ?, ?, ?, ?, ?, ?, ?, ?)\n" +
+                " on duplicate key update `id`=?;";
+        this.jdbcTemplate.update(sql, hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
+                hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(), hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(), hospital.getRoadNameAddress(), hospital.getHospitalName(),
+                hospital.getBusinessTypeName(), hospital.getHealthcareProviderCnt(), hospital.getPatientRoomCnt(), hospital.getTotalNumberOfBeds(), hospital.getTotalAreaSize(), hospital.getId());
+    }
 }
