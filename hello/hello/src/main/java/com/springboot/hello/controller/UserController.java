cef97f21a8e7fde5a345c82d6be0894124aa5a89
이소영 <th42500@naver.com>
2022-11-01 10:02:49 +0900

    [Add] UserController 로그 추가해보기

--- hello/hello/src/main/java/com/springboot/hello/controller/UserController.java
+++ hello/hello/src/main/java/com/springboot/hello/controller/UserController.java
@@ -3,12 +3,13 @@ package com.springboot.hello.controller;
 import com.springboot.hello.domain.User;
 import com.springboot.hello.domain.dao.UserDao;
 import com.springboot.hello.domain.dto.UserRegisterPostReq;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Slf4j
 public class UserController {
 
     private final UserDao userDao;
@@ -20,6 +21,7 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<Integer> register(@RequestBody UserRegisterPostReq registerInfo) {
         User user = new User(registerInfo.getId(), registerInfo.getName(), registerInfo.getPassword());
+        log.info(registerInfo.toString() + "를 호출하였습니다.");
         userDao.add(user);
         return ResponseEntity
                 .ok()
