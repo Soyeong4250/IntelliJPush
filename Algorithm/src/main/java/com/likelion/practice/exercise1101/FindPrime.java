780c30dee592b0a331ba815d504024bddf125214
이소영 <th42500@naver.com>
2022-11-01 10:17:35 +0900

    [Add] 소수 판별 2,3 추가
    1. n/2미만까지 나누어 보기
    2. √n까지 나누어 보기

--- Algorithm/src/main/java/com/likelion/practice/exercise1101/FindPrime.java
+++ Algorithm/src/main/java/com/likelion/practice/exercise1101/FindPrime.java
@@ -9,9 +9,29 @@ public class FindPrime {
         return true;
     }
 
+    // 소수 판별 방법 2
+    public boolean isPrime2(int num) {
+        for (int i = 2; i < num/2; i++) {
+            if(num % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    // 소수 판별 방법 3
+    public boolean isPrime3(int num) {
+        for (int i = 2; i*i < num; i++) {
+            if(num % i == 0) return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         FindPrime main = new FindPrime();
 
         System.out.println(main.isPrime(13));
+        System.out.println(main.isPrime2(13));
+        System.out.println(main.isPrime3(13));
     }
 }
