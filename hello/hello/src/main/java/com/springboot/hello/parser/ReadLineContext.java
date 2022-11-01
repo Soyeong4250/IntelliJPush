961a1cf59610fe4c87649b6e3cd8c56a9541daf1
이소영 <th42500@naver.com>
2022-11-01 10:01:18 +0900

    [Fix] ReadLineContext 한글 깨짐으로 인한 데이터 인코딩 설정

--- hello/hello/src/main/java/com/springboot/hello/parser/ReadLineContext.java
+++ hello/hello/src/main/java/com/springboot/hello/parser/ReadLineContext.java
@@ -1,9 +1,10 @@
 package com.springboot.hello.parser;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +18,19 @@ public class ReadLineContext<T> {
 
     public List<T> readLineParser(String filename) throws IOException {
         List<T> result = new ArrayList<>();
-        BufferedReader br = new BufferedReader( new FileReader(filename));
-
+        // BufferedReader br = new BufferedReader(new FileReader(filename));
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "euc-kr"));
         String str;
-        while((str = br.readLine())!=null) {
-            result.add(parser.parse(str));
+        String temp = br.readLine();  // 첫 한줄 (제목 행) 날리기
+        System.out.println(temp);
+        while((str = br.readLine()) != null) {
+            System.out.printf("%s \n", str);
+            try {
+                result.add(parser.parse(str));
+                System.out.println("성공");
+            }catch (Exception e) {
+                System.out.printf("파싱 중 문제발생! 파일내용: %s\n", str.substring(0, 20));
+            }
         }
         br.close();
         return result;
