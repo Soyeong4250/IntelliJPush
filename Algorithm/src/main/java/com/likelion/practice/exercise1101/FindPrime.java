4a0bd6a941b6d94ae008d6090bef0e4b5caa665f
이소영 <th42500@naver.com>
2022-11-01 10:20:58 +0900

    [Refactor] 템플릿콜백패턴 적용

--- Algorithm/src/main/java/com/likelion/practice/exercise1101/FindPrime.java
+++ Algorithm/src/main/java/com/likelion/practice/exercise1101/FindPrime.java
@@ -1,37 +1,21 @@
 package com.likelion.practice.exercise1101;
 
 public class FindPrime {
-    // 소수 판별 방법 1
-    public boolean isPrime(int num) {
-        for (int i = 2; i < num; i++) {
-            if(num % i == 0) return false;
-        }
-        return true;
+    // 템플릿, 콜백 적용
+    boolean someOperation(int a, int b) {
+        return a < b;
     }
 
-    // 소수 판별 방법 2
-    public boolean isPrime2(int num) {
-        for (int i = 2; i < num/2; i++) {
+    boolean isPrime(int num) {
+        for (int i = 0; someOperation(i, num); i++) {
             if(num % i == 0) return false;
         }
-
-        return true;
-    }
-
-    // 소수 판별 방법 3
-    public boolean isPrime3(int num) {
-        for (int i = 2; i*i < num; i++) {
-            if(num % i == 0) return false;
-        }
-
         return true;
     }
 
     public static void main(String[] args) {
         FindPrime main = new FindPrime();
 
-        System.out.println(main.isPrime(13));
-        System.out.println(main.isPrime2(13));
-        System.out.println(main.isPrime3(13));
+        System.out.println(main.isPrime(17));
     }
 }
