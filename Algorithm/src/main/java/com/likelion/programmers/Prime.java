b2bbb638b06256445d7da3dd144f2ea7489726fb
이소영 <th42500@naver.com>
2022-11-02 09:44:06 +0900

    [Add] 프로그래머스 소수 찾기 풀이 코드 (에라토스테네스의 체)

--- Algorithm/src/main/java/com/likelion/programmers/Prime.java
+++ Algorithm/src/main/java/com/likelion/programmers/Prime.java
@@ -3,12 +3,30 @@ package com.likelion.programmers;
 public class Prime {
     private int solution(int n) {
         // 에라토스테네스의 체
-        int cnt = 0;
-        /*
+         /*
         1. 소수인지 아닌지 판단하는 boolean 타입의 배열 선언
         2. 2~n까지 2중 for문을 순회하면서 i의 배수라면 소수 표시(isNotPrime = true)
         3. for문을 이용하여 boolean 타입 배열을 순회하며 isNotPrime = false인 것의 개수 세기
          */
+        int cnt = 0;
+
+        boolean[] isNotPrime = new boolean[n+1];
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        for(int i=2; i<=n; i++) {
+            if(!isNotPrime[i]) {
+                for (int j=i*2; j<=n; j+=i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < isNotPrime.length; i++) {
+            if(!isNotPrime[i]) {
+                cnt++;
+            }
+        }
 
         return cnt;
     }
