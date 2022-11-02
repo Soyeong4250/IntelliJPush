1454b564300c4917337127d5e1bd042809857db4
이소영 <th42500@naver.com>
2022-11-02 13:47:38 +0900

    [Refactor] 사용하지 않는 클래스 호출 제거

--- hello/hello/src/main/java/com/springboot/hello/parser/ParserFactory.java
+++ hello/hello/src/main/java/com/springboot/hello/parser/ParserFactory.java
@@ -1,7 +1,6 @@
 package com.springboot.hello.parser;
 
 import com.springboot.hello.domain.Hospital;
-import com.springboot.hello.domain.dao.HospitalDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
