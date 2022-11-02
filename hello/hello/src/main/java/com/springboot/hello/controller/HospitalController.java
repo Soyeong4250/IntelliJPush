44c06bc2c037cb69718b436ed06f45df3e157b72
이소영 <th42500@naver.com>
2022-11-02 12:30:47 +0900

    [Add] hospital의 id 검색 기능 추가

---
+++ hello/hello/src/main/java/com/springboot/hello/controller/HospitalController.java
@@ -0,0 +1,32 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.domain.Hospital;
+import com.springboot.hello.domain.dao.HospitalDao;
+import com.springboot.hello.domain.dto.HospitalDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/hospital")
+public class HospitalController {
+
+    private final HospitalDao hospitalDao;
+
+    public HospitalController(HospitalDao hospitalDao) {
+        this.hospitalDao = hospitalDao;
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Hospital> findById(@PathVariable int id) {
+        Hospital hospital = hospitalDao.selectById(id);
+
+        if(hospital == null) {
+            return ResponseEntity.notFound().build();
+        }
+        hospitalDao.add(hospital);
+        return ResponseEntity
+                .ok()
+                .body(hospital);
+    }
+}
