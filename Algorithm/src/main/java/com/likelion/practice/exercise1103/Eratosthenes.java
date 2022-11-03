656adc18e6f1d1cc1296c155ab841e7fcd0b69d4
이소영 <th42500@naver.com>
2022-11-03 09:59:57 +0900

    [Refactor] 에라토스테네스의 체 원리 깨닫기 2단계 (초기 세팅)
    문제 풀기 전 초기 세팅을 위해 nums 배열에는 2~49까지, checks배열에는 true를 채워넣기

--- Algorithm/src/main/java/com/likelion/practice/exercise1103/Eratosthenes.java
+++ Algorithm/src/main/java/com/likelion/practice/exercise1103/Eratosthenes.java
@@ -1,9 +1,24 @@
 package com.likelion.practice.exercise1103;
 
+import java.util.Arrays;
+
 public class Eratosthenes {
     private void multipleOf(int num) {
-        for(int i = 2; i*2<=num; i++) {
-            System.out.printf("%d, %d\n", i, (2*i) + 2);
+        int N = num/2;  // 2씩 증가할 예정이므로 2로 나누어주기
+        int[] nums = new int[N-1];
+        boolean[] checks = new boolean[nums.length];
+
+        Arrays.fill(checks, true);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 2;
+        }
+
+        print(nums, checks);
+    }
+
+    public void print(int[] nums, boolean[] checks) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.printf("%d %s\n", nums[i], checks[i]);
         }
     }
 
