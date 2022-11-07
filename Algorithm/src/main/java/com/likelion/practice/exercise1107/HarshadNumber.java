32253227db910634459a92cecadeaef25ea4912a
이소영 <th42500@naver.com>
2022-11-07 09:39:02 +0900

    [Add] 프로그래머스 하샤드 수 String 타입으로 변환해서 풀어보기

--- Algorithm/src/main/java/com/likelion/practice/exercise1107/HarshadNumber.java
+++ Algorithm/src/main/java/com/likelion/practice/exercise1107/HarshadNumber.java
@@ -4,18 +4,36 @@ public class HarshadNumber {
 
     private boolean isHarshad(int x) {
         boolean result = true;
-        
+
         // 1. 자릿수의 합 구하기
         int sum = 0;
         int remainder = x;
-        while(remainder > 0) {
-            sum += remainder%10;
+        while (remainder > 0) {
+            sum += remainder % 10;
             remainder /= 10;
         }
 
         // 2. 나누어 떨어지는지 check 하기
-        if(x%sum != 0) {
+        if (x % sum != 0) {
             result = false;
+        }
+
+        return result;
+    }
+
+    private boolean isHarshad2(int x) {
+        boolean result = true;
+
+        // 1. 자릿수의 합 구하기
+        int sum = 0;
+        String num = String.valueOf(x);
+        for (int i = 0; i < num.length(); i++) {
+            sum += num.charAt(i) - '0';
+        }
+
+        // 2. 나누어 떨어지는지 check 하기
+        if (x % sum != 0) {
+            return false;
         }
 
         return result;
@@ -28,5 +46,9 @@ public class HarshadNumber {
         System.out.println(harshadNumber.isHarshad(12));  // true
         System.out.println(harshadNumber.isHarshad(11));  // false
         System.out.println(harshadNumber.isHarshad(13));  // false
+        System.out.println(harshadNumber.isHarshad2(10));  // true
+        System.out.println(harshadNumber.isHarshad2(12));  // true
+        System.out.println(harshadNumber.isHarshad2(11));  // false
+        System.out.println(harshadNumber.isHarshad2(13));  // false
     }
 }
