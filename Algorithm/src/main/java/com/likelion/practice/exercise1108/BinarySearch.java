c628b3f2fb32ae0dcf0b2b64571448538ba342e6
이소영 <th42500@naver.com>
2022-11-08 10:41:55 +0900

    [Refactor] 이분탐색 구현 중간값 구하는 코드 수정

--- Algorithm/src/main/java/com/likelion/practice/exercise1108/BinarySearch.java
+++ Algorithm/src/main/java/com/likelion/practice/exercise1108/BinarySearch.java
@@ -10,13 +10,15 @@ public class BinarySearch {
         while(start <= end) {
             cnt++;
             // 1. 중간값 찾기
-            int median = (start + end)/2 - 1;
+            int median = (start + end)/2;
             // 2. 인덱스(시작점, 끝점 옮기기)
             // 3. targetNum과 같은지 비교하기
             if(targetNum < nums[median]) {
                 end = median - 1;
+                System.out.println("끝점을 " + end + "(으)로 옮깁니다.");
             } else if(nums[median] < targetNum) {
                 start = median + 1;
+                System.out.println("시작점을 " + start + "(으)로 옮깁니다.");
             } else if(nums[median] == targetNum) {
                 System.out.println(cnt + "번만에 " + targetNum + "을 찾았습니다");
                 break;
@@ -28,6 +30,6 @@ public class BinarySearch {
         BinarySearch binarySearch = new BinarySearch();
         int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-        binarySearch.solution(nums, 7);
+        binarySearch.solution(nums, 8);
     }
 }
