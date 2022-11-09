74594a416d1699fc9d57c6ee4d1dced293072f27
이소영 <th42500@naver.com>
2022-11-09 10:50:44 +0900

    [Refactor] 선택 정렬 - BiFunction 적용하기

--- Algorithm/src/main/java/com/likelion/practice/exercise1109/SelectionSort.java
+++ Algorithm/src/main/java/com/likelion/practice/exercise1109/SelectionSort.java
@@ -1,15 +1,32 @@
 package com.likelion.practice.exercise1109;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 interface StatementStrategy {
-    boolean apply(int a, int b);
+    boolean compare(int a, int b);
 }
 
 public class SelectionSort {
     private int solution(int[] numArr, int idx, StatementStrategy stmt) {
         System.out.println(Arrays.toString(numArr));
         for (int i = 0; i < numArr.length-1; i++) {
+            int targetIdx = i;
+            for (int j = i+1; j < numArr.length; j++) {
+                if(stmt.compare(numArr[targetIdx], numArr[j])) {
+                    targetIdx = j;
+                }
+            }
+            swapValue(numArr, i, targetIdx);
+            System.out.println(Arrays.toString(numArr));
+        }
+
+        return numArr[idx];
+    }
+
+    public int solution2(int[]numArr, int idx, BiFunction<Integer, Integer, Boolean> stmt) {
+        for (int i = 0; i < numArr.length - 1; i++) {
             int targetIdx = i;
             for (int j = i+1; j < numArr.length; j++) {
                 if(stmt.apply(numArr[targetIdx], numArr[j])) {
@@ -38,5 +55,11 @@ public class SelectionSort {
         // 그 값이 들어있는 idx 찾기
         System.out.println(selectionSort.solution(numArr, 0, (a, b) -> a > b));  // 오름차순
         System.out.println(selectionSort.solution(numArr, 0, (a, b) -> a < b));  // 내림차순
+
+        Function<Integer[], Boolean> fn = (arr1) -> arr1[0] > arr1[1];
+        System.out.println(fn.apply(new Integer[]{10, 20}));
+
+        BiFunction<Integer, Integer, Boolean> biFunction = (a, b) -> a > b;   // 오름차순
+        System.out.println(selectionSort.solution2(numArr, 0, biFunction));
     }
 }
