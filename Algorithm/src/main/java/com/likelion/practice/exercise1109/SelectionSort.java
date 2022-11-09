5d1a222c5691668e7e209bc34525d003b5ca2170
이소영 <th42500@naver.com>
2022-11-09 09:42:37 +0900

    [Add] 선택 정렬 내림차순 구현 완료

--- Algorithm/src/main/java/com/likelion/practice/exercise1109/SelectionSort.java
+++ Algorithm/src/main/java/com/likelion/practice/exercise1109/SelectionSort.java
@@ -3,7 +3,8 @@ package com.likelion.practice.exercise1109;
 import java.util.Arrays;
 
 public class SelectionSort {
-    private int solution(int[] numArr, int idx) {
+    // 오름차순
+    private int solution1(int[] numArr, int idx) {
         System.out.println(Arrays.toString(numArr));
         for (int i = 0; i < numArr.length-1; i++) {
             int minIdx = i;
@@ -21,10 +22,29 @@ public class SelectionSort {
         return numArr[idx];
     }
 
-    private void swapValue(int[] numArr, int i, int minIdx) {
+    // 내림차순
+    private int solution2(int[] numArr, int idx) {
+        System.out.println(Arrays.toString(numArr));
+        for (int i = 0; i < numArr.length-1; i++) {
+            int maxIdx = i;
+            for (int j = i+1; j < numArr.length; j++) {
+                if(numArr[maxIdx] < numArr[j]) {
+                    maxIdx = j;
+                }
+            }
+            if(numArr[i] < numArr[maxIdx]) {
+                swapValue(numArr, i, maxIdx);
+            }
+            System.out.println(Arrays.toString(numArr));
+        }
+
+        return numArr[idx];
+    }
+
+    private void swapValue(int[] numArr, int i, int targetIdx) {
         int temp = numArr[i];
-        numArr[i] = numArr[minIdx];
-        numArr[minIdx] = temp;
+        numArr[i] = numArr[targetIdx];
+        numArr[targetIdx] = temp;
     }
 
     public static void main(String[] args) {
@@ -34,6 +54,7 @@ public class SelectionSort {
 
         // arr[0]번째에 들어갈 값 찾기
         // 그 값이 들어있는 idx 찾기
-        System.out.println(selectionSort.solution(numArr, 0));
+        System.out.println(selectionSort.solution1(numArr, 0));
+        System.out.println(selectionSort.solution2(numArr, 0));
     }
 }
