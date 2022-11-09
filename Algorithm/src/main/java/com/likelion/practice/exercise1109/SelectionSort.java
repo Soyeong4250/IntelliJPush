897caa3ef429deac1e0697481d9bd14b7d3c33c3
이소영 <th42500@naver.com>
2022-11-09 09:29:21 +0900

    [Add] 선택 정렬(SelectionSort) 구현

---
+++ Algorithm/src/main/java/com/likelion/practice/exercise1109/SelectionSort.java
@@ -0,0 +1,36 @@
+package com.likelion.practice.exercise1109;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    private int solution(int[] numArr, int idx) {
+        System.out.println(Arrays.toString(numArr));
+        for (int i = 0; i < numArr.length-1; i++) {
+            int maxIdx = i;
+            for (int j = i+1; j < numArr.length; j++) {
+                if(numArr[maxIdx] < numArr[j]) {
+                    maxIdx = j;
+                }
+            }
+            if(numArr[i] < numArr[maxIdx]) {
+                swapValue(numArr, i, maxIdx);
+            }
+            System.out.println(Arrays.toString(numArr));
+        }
+
+        return numArr[0];
+    }
+
+    private void swapValue(int[] numArr, int i, int maxIdx) {
+        int temp = numArr[i];
+        numArr[i] = numArr[maxIdx];
+        numArr[maxIdx] = temp;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort selectionSort = new SelectionSort();
+
+        int[] numArr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        System.out.println(selectionSort.solution(numArr, 0));
+    }
+}
