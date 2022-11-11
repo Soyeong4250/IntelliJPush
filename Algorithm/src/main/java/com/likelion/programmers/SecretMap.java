17a06f202d75e7ea5d7b694f8a174c84474dff4b
이소영 <th42500@naver.com>
2022-11-11 10:25:14 +0900

    [Refactor] 프로그래머스 - [1차] 비밀지도 반환값 수정에 의한 코드 변경

--- Algorithm/src/main/java/com/likelion/programmers/SecretMap.java
+++ Algorithm/src/main/java/com/likelion/programmers/SecretMap.java
@@ -3,8 +3,8 @@ package com.likelion.programmers;
 import java.util.Arrays;
 
 public class SecretMap {  // 비밀지도
-    private char[][] solution(int n, int[] arr1, int[] arr2) {
-        char[][] resultMap = new char[n][n];
+    private String[] solution(int n, int[] arr1, int[] arr2) {
+        char[][] map = new char[n][n];
 
         // 1. Integer.toBinaryString()을 이용하여 arr1과 arr2의 각 수를 2진수 문자열로 바꾸기
         for (int i = 0; i < n; i++) {
@@ -15,12 +15,14 @@ public class SecretMap {  // 비밀지도
             for (int j = 0; j < n; j++) {
                 // 3. 두 배열 중 1개라도 1이라면 resultMap에 #표시
                 if(binary1.charAt(j) == '1' || binary2.charAt(j) == '1') {
-                    resultMap[i][j] = '#';
+                    map[i][j] = '#';
                 } else {  // 4. 두 배열 모두 0이라면 resultMap에 공백 표시
-                    resultMap[i][j] = ' ';
+                    map[i][j] = ' ';
                 }
             }
         }
+
+        String[] resultMap = makeStringArr(map);
 
         return resultMap;
     }
@@ -36,9 +38,21 @@ public class SecretMap {  // 비밀지도
         return binary;
     }
 
-    private void printArr(char[][] resultMap) {
+    private String[] makeStringArr(char[][] map) {
+        String[] resultMap = new String[map.length];
+        for (int i = 0; i < map.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < map[i].length; j++) {
+                sb.append(map[i][j]);
+            }
+            resultMap[i] = sb.toString();
+        }
+        return resultMap;
+    }
+
+    private void printArr(String[] resultMap) {
         for (int i = 0; i < resultMap.length; i++) {
-            System.out.println(Arrays.toString(resultMap[i]));
+            System.out.println(resultMap[i]);
         }
     }
 
@@ -48,7 +62,7 @@ public class SecretMap {  // 비밀지도
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {30, 1, 21, 17, 28};
 
-        char[][] resultMap = main.solution(n, arr1, arr2);
+        String[] resultMap = main.solution(n, arr1, arr2);
         main.printArr(resultMap);
     }
 
