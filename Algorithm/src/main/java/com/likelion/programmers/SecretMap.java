01f3ca92f7e702de215be5575a7eeab189798908
이소영 <th42500@naver.com>
2022-11-11 09:35:52 +0900

    [Feat] 프로그래머스 - [1차] 비밀지도 구현 중 SringIndexOutOfBoundsException 발생

--- Algorithm/src/main/java/com/likelion/programmers/SecretMap.java
+++ Algorithm/src/main/java/com/likelion/programmers/SecretMap.java
@@ -7,10 +7,20 @@ public class SecretMap {  // 비밀지도
         char[][] resultMap = new char[n][n];
 
         // 1. Integer.toBinaryString()을 이용하여 arr1과 arr2의 각 수를 2진수 문자열로 바꾸기
-        // 2. 반복문 2개를 이용하여 arr1과 arr2의 각 행의 문자열을 한글자씩 순회하기
-        // 3. 두 배열 중 1개라도 1이라면 resultMap에 #표시
-        // 4. 두 배열 모두 0이라면 resultMap에 공백 표시
+        for (int i = 0; i < n; i++) {
+            String binary1 = Integer.toBinaryString(arr1[i]);
+            String binary2 = Integer.toBinaryString(arr2[i]);
 
+            // 2. 반복문 2개를 이용하여 arr1과 arr2의 각 행의 문자열을 한글자씩 순회하기
+            for (int j = 0; j < n; j++) {
+                // 3. 두 배열 중 1개라도 1이라면 resultMap에 #표시
+                if(binary1.charAt(j) == '1' || binary2.charAt(j) == '1') {
+                    resultMap[i][j] = '#';
+                } else {  // 4. 두 배열 모두 0이라면 resultMap에 공백 표시
+                    resultMap[i][j] = ' ';
+                }
+            }
+        }
 
         return resultMap;
     }
