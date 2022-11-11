d6a2e9416c13e59d7126cd912227b4f0acc22b26
이소영 <th42500@naver.com>
2022-11-11 09:30:04 +0900

    [Feat] 10진수를 2진수로 바꾸는 메서드 구현해보기

---
+++ Algorithm/src/main/java/com/likelion/practice/exercise1110/IntToBinary.java
@@ -0,0 +1,24 @@
+package com.likelion.practice.exercise1110;
+
+import java.util.Scanner;
+
+public class IntToBinary {  // 10진수를 2진수로 바꾸어보기
+    private String toBinary(int num) {
+        StringBuilder sb = new StringBuilder();
+
+        while(num > 0) {
+            sb.append(num%2);
+            num /= 2;
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        IntToBinary main = new IntToBinary();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num + "을 2진수로 바꾼 문자열은 " + main.toBinary(num) + "입니다.");
+    }
+
+}
