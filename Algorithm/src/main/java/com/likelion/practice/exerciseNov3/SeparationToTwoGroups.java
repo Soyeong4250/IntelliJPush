308411be303bbc10e3573afef24e352ddfb7f49b
이소영 <th42500@naver.com>
2022-11-16 10:19:29 +0900

    [Refactor] 배열을 이용한 퀵 정렬 - pivot 기준 왼쪽, 오른쪽 크기 비교하여 swap
    1. pivot의 왼쪽 값들은 pivot보다 작으면 start++
    2. pivot의 오른쪽 값들은 pivot보다 크면 end--
    3. 변한 start와 end 인덱스 값이 start<=end라면 swap하기

--- Algorithm/src/main/java/com/likelion/practice/exerciseNov3/SeparationToTwoGroups.java
+++ Algorithm/src/main/java/com/likelion/practice/exerciseNov3/SeparationToTwoGroups.java
@@ -8,27 +8,28 @@ public class SeparationToTwoGroups {
         int start = 0;
         int end = arr.length-1;
 
-        // pivot 왼쪽 값들은 pivot보다 작으면 swap ❌ startIdx++
-        while(start <= median) {
-            if(arr[start] > pivot) {
-                swap(arr, start, median);
-                System.out.println("바꿨습니다");
-            }
+        // pivot 왼쪽 값들은 pivot보다 작으면 swap ❌ start++
+        while(arr[start] < pivot) start++;
+        // pivot 오른쪽 값들은 pivot보다 크면 swap ❌ end--
+        while (arr[end] > pivot) end--;
+        if (start <= end) {
+            swap(arr, start, end);
             start++;
-            System.out.println(Arrays.toString(arr));
+            end--;
         }
+        System.out.println(Arrays.toString(arr));
 
     }
 
-    private void swap(int[] arr, int idx, int median) {
-        int temp = arr[idx];
-        arr[idx] = arr[median];
-        arr[median] = temp;
+    private void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 
     public static void main(String[] args) {
         SeparationToTwoGroups main = new SeparationToTwoGroups();
-        var arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        var arr = new int[]{20, 60, 5, 19, 40, 50, 5, 45};
 
         int median = arr.length/2;
         main.solution(arr, median);
