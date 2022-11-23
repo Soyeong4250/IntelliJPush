3fdf54e9b983bc93154774cbef06067eeb52af86
이소영 <th42500@naver.com>
2022-11-23 10:54:46 +0900

    [Refactor] 코드업 - 재귀로 n번째 피보나치 수 리턴하기 핵심로직 위치 변경

--- CodeUp/src/Main1855.java
+++ CodeUp/src/Main1855.java
@@ -1,13 +1,6 @@
 import java.util.Scanner;
 
 public class Main1855 {  // [기초-재귀함수] 재귀로 n번째 피보나치 수 리턴하기
-    public static void main(String[] args) {
-        Main1855 main = new Main1855();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(main.fibo(n));
-        System.out.println(main.dp(n));
-    }
 
     private long dp(int n) {
         long[] fibo = new long[n+1];
@@ -29,5 +22,13 @@ public class Main1855 {  // [기초-재귀함수] 재귀로 n번째 피보나치
 
         long result = fibo(n-1) + fibo(n-2);
         return result;
+    }
+
+    public static void main(String[] args) {
+        Main1855 main = new Main1855();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(main.fibo(n));
+        System.out.println(main.dp(n));
     }
 }
