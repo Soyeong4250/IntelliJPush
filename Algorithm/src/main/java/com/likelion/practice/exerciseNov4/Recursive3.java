042e06ada4f34e489f59287d0724c740c1f2faf1
이소영 <th42500@naver.com>
2022-11-25 10:52:18 +0900

    [Add] 꼬리재귀 빼기 구현 (while문 이용)

--- Algorithm/src/main/java/com/likelion/practice/exerciseNov4/Recursive3.java
+++ Algorithm/src/main/java/com/likelion/practice/exerciseNov4/Recursive3.java
@@ -5,12 +5,21 @@ public class Recursive3 {  // 꼬리재귀 빼기
         if(n <= 0) return;
         recur(n - 1);
         System.out.println(n);
-        recur(n-2);
+        recur(n - 2);
+    }
+
+    private void recur2(int n) {
+        while(n > 0) {
+            recur(n - 1);
+            System.out.println(n);
+            n = n - 2;
+        }
     }
 
     public static void main(String[] args) {
         Recursive3 main = new Recursive3();
 
         main.recur(3);
+        main.recur2(3);
     }
 }
