0d337bb27b6ec3ff87991d7b8591980ba173b18a
이소영 <th42500@naver.com>
2022-11-28 10:20:53 +0900

    [Refactor] AA ~ ZZ 문자열 조합 출력하기 코드 수정
    외부에서 입력받은 수로 문자열 조합 길이를 구할 수 있도록 수정

--- Algorithm/src/main/java/com/likelion/practice/exerciseNov5/CombinationOfAlphabet.java
+++ Algorithm/src/main/java/com/likelion/practice/exerciseNov5/CombinationOfAlphabet.java
@@ -15,25 +15,25 @@ public class CombinationOfAlphabet {
         }
     }
 
-    private void combination2(String prefix) {
-        if(prefix.length() == 3) {
+    private void combination2(String prefix, int select) {
+        if(prefix.length() == select) {
             System.out.println(prefix);
             return;
         }
 
         for (char ch = 'A'; ch <= 'Z'; ch++) {
-            combination2(prefix + ch);
+            combination2(prefix + ch, select);
         }
     }
 
     public static void main(String[] args) {
         CombinationOfAlphabet main = new CombinationOfAlphabet();
-        int select = 3;
+        int select = 2;
         char[] alphabet = new char[select];
         System.out.println("-----conbination-----");
         main.combination(alphabet, 0);
         System.out.println(answer.toString());
         System.out.println("-----conbination2-----");
-        main.combination2("");
+        main.combination2("", select);
     }
 }
