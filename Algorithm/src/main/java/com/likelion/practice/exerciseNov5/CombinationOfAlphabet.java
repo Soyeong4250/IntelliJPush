e4c0cced1afde1056a58dba1211006e14ecc07e4
이소영 <th42500@naver.com>
2022-11-28 09:38:03 +0900

    [Refactor] AA ~ ZZ 재귀 이용하여 출력하기 문자 간 공백 제거

--- Algorithm/src/main/java/com/likelion/practice/exerciseNov5/CombinationOfAlphabet.java
+++ Algorithm/src/main/java/com/likelion/practice/exerciseNov5/CombinationOfAlphabet.java
@@ -3,7 +3,7 @@ package com.likelion.practice.exerciseNov5;
 public class CombinationOfAlphabet {
     static StringBuilder answer = new StringBuilder();
     private void combination(char[] alphabet, int idx) {
-        if(idx > alphabet.length) {
+        if(idx == alphabet.length) {
             answer.append(new String(alphabet));
             answer.append('\n');
             return;
@@ -11,17 +11,14 @@ public class CombinationOfAlphabet {
 
         for (int i = 65; i < 91; i++) {
             alphabet[idx] = (char)i;
-            combination(alphabet, idx+2);
+            combination(alphabet, idx+1);
         }
     }
 
     public static void main(String[] args) {
         CombinationOfAlphabet main = new CombinationOfAlphabet();
         int select = 2;
-        char[] alphabet = new char[2*select-1];
-        for (int i = 1; i < alphabet.length; i+=2) {
-            alphabet[i] = ' ';
-        }
+        char[] alphabet = new char[select];
         main.combination(alphabet, 0);
         System.out.println(answer.toString());
     }
