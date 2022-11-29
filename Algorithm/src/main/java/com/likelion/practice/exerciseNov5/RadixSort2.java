5b24d0348b50735a819003a9c3d1a2184cf057a4
이소영 <th42500@naver.com>
2022-11-29 11:08:33 +0900

    [Add] 자릿수별로 수를 분류 & Queue에 담긴 값들을 꺼내서 배열로 만들기

--- Algorithm/src/main/java/com/likelion/practice/exerciseNov5/RadixSort2.java
+++ Algorithm/src/main/java/com/likelion/practice/exerciseNov5/RadixSort2.java
@@ -18,13 +18,39 @@ public class RadixSort2 {
         }
 
         Arrays.sort(digits);
+
         return digits;
+    }
+
+    private int[] sort(int[] arr, int digit) {
+        Queue<Integer>[] qArr = new Queue[10];
+        for (int i = 0; i < qArr.length; i++) {
+            qArr[i] = new ArrayDeque<>();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int divisor = (int)Math.pow(10, digit - 1);
+            qArr[Math.floorDiv(arr[i], divisor) % 10].add(arr[i]);
+        }
+
+        int idx = 0;
+        for (int i = 0; i < qArr.length; i++) {
+            while(!qArr[i].isEmpty()) {
+                arr[idx++] = qArr[i].poll();
+            }
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
         RadixSort2 main = new RadixSort2();
 
         int[] arr = new int[]{7, 4, 5, 9, 1, 0};
-        System.out.println(Arrays.toString(main.getDigits(arr)));
+        int[] digits = main.getDigits(arr);
+        for (var digit : digits) {
+            arr = main.sort(arr, digit);
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
