35ef661490958806946243bd51eec87e2ae8660c
이소영 <th42500@naver.com>
2022-12-06 10:41:54 +0900

    [Feat] Heap 정렬 구현하기

--- Algorithm/src/main/java/com/likelion/practice/exerciseDec1/Heap.java
+++ Algorithm/src/main/java/com/likelion/practice/exerciseDec1/Heap.java
@@ -3,19 +3,19 @@ package com.likelion.practice.exerciseDec1;
 import java.util.Arrays;
 
 public class Heap {
-    private int[] makeHeap(int[] arr, int parentIdx) {
+    private int[] makeHeap(int[] arr, int parentIdx, int arrSize) {
 
         int leftIdx = 2 * parentIdx + 1;
         int rightIdx = 2 * parentIdx + 2;
         int greaterIdx = parentIdx;
 
         // 왼쪽이 parent보다 크면 greaterIdx = leftIdx
-        if(leftIdx < arr.length && arr[leftIdx] > arr[greaterIdx]) {
+        if(leftIdx < arrSize && arr[leftIdx] > arr[greaterIdx]) {
             greaterIdx = leftIdx;
         }
 
         // 오른쪽 자식이 greater보다 크면
-        if(rightIdx < arr.length && arr[rightIdx] > arr[greaterIdx]) {
+        if(rightIdx < arrSize && arr[rightIdx] > arr[greaterIdx]) {
             greaterIdx = rightIdx;
         }
 
@@ -24,7 +24,7 @@ public class Heap {
             int temp = arr[parentIdx];
             arr[parentIdx] = arr[greaterIdx];
             arr[greaterIdx] = temp;
-            makeHeap(arr, greaterIdx);
+            makeHeap(arr, greaterIdx, arrSize);
         }
 
         return arr;
@@ -33,10 +33,20 @@ public class Heap {
     public static void main(String[] args) {
         Heap main = new Heap();
 
-        int[] arr = {6, 5, 7, 8};
+//        int[] arr = {6, 5, 7, 8};
+        int[] arr = {10, 9, 5, 8, 3, 2, 4, 6, 7, 1};
 
         for (int i = (arr.length-2)/2; i >= 0; i--) {
-            arr = main.makeHeap(arr, i);
+            arr = main.makeHeap(arr, i, arr.length);
+            System.out.println(Arrays.toString(arr));
+        }
+
+        // 정렬
+        for (int i =arr.length-1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            arr = main.makeHeap(arr, 0, i);
             System.out.println(Arrays.toString(arr));
         }
     }
