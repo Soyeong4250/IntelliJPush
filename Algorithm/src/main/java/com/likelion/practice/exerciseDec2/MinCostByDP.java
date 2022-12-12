dc48afeb20e3031c229d66008bd5c0655a7d3630
이소영 <th42500@naver.com>
2022-12-12 09:22:12 +0900

    [Add] DP를 이용한 최소 비용 구하기 - dp memo배열 선언

--- Algorithm/src/main/java/com/likelion/practice/exerciseDec2/MinCostByDP.java
+++ Algorithm/src/main/java/com/likelion/practice/exerciseDec2/MinCostByDP.java
@@ -1,11 +1,21 @@
 package com.likelion.practice.exerciseDec2;
 
+import java.util.Arrays;
+
 public class MinCostByDP {
     private int solution(int[][] arr) {
         int[][] memo = new int[arr.length][arr[0].length];
 
+        memo[0][0] = arr[0][0];
 
+        print(memo);
         return 0;
+    }
+
+    private void print(int[][] memo) {
+        for (int i = 0; i < memo.length; i++) {
+            System.out.println(Arrays.toString(memo[i]));
+        }
     }
 
     public static void main(String[] args) {
